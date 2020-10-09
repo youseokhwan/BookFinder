96432e66e2e241232245b46d3b2d9c39978a428d
youseokhwan <tjrghks115@naver.com>
2020-10-10 04:17:38 +0900

    Update settings.py

--- midterm/midterm/settings.py
+++ midterm/midterm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!lh%zg+i2bzt2s&s9=5#a@j&^cs@_ps#f#6e)k1sa2ctb=kv*8'
+SECRET_KEY = 'PRIMARY_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
